00ae5ffe2ff726f982e4be6b488d32101109c2cf
DumplingDefender <82426255+DumplingDefender@users.noreply.github.com>
2023-11-26 17:23:05 -0600

    Initial Commit

---
+++ BPA-MobileApps/BPA_MobileAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPA_MobileAppsApp.swift
+//  BPA-MobileApps
+//
+//  Created by Krish Bothra on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BPA_MobileAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
